4d12227ad93e7d35db2842dea09645523c3475af
Pavan Ingale <pavaningale464@gmail.com>
2025-12-08 20:36:49 +0530

    This is String reverse Program

---
+++ ReversStr.java
@@ -0,0 +1,13 @@
+public class ReversStr {
+    public static void main(String[] args) {
+        String a="How Are You";
+        System.out.println(a);
+
+        String rev= new StringBuilder(a).reverse().toString();
+
+        System.out.println(rev);
+            
+         
+        }
+
+}
